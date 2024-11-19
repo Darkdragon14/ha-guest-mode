8e092ad9a8bd4e522da91495cd7d2a364c551406
Darkdragon14 <s.tristan@hotmail.fr>
2024-11-19 22:05:48 +0100

    feat(customize-tab): change config flow to customize tab name and path

--- custom_components/ha_guest_mode/config_flow.py
+++ custom_components/ha_guest_mode/config_flow.py
@@ -1,5 +1,5 @@
 from homeassistant import config_entries
-from homeassistant.core import callback
+import voluptuous as vol
 
 from .const import DOMAIN  # Assurez-vous que DOMAIN est défini dans const.py
 
@@ -9,9 +9,19 @@ class GuestModeConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
+    def __init__(self):
+        self.data = {}
+
     async def async_step_user(self, user_input=None):
         """Handle the initial step."""
         if user_input is not None:
-            return self.async_create_entry(title="Guest Mode", data={})
+            self.data.update(user_input)
+            return self.async_create_entry(title="Guest Mode", data=self.data)
 
-        return self.async_show_form(step_id="user")
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema({
+                vol.Optional("tab_name", default="Guest"): str,
+                vol.Optional("path", default="/guest-mode"): str,
+            }),
+        )
