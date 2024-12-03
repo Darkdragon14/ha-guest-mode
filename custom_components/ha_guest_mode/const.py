004881cc1a1e6a79ce9dfb9d7dad17b62827d28c
Darkdragon14 <s.tristan@hotmail.fr>
2024-12-03 21:59:32 +0100

    feat(options_flow): add option flow to change icon, title tab and path

--- custom_components/ha_guest_mode/const.py
+++ custom_components/ha_guest_mode/const.py
@@ -1,1 +1,12 @@
 DOMAIN = "ha_guest_mode"
+DATABASE = "/config/custom_components/ha_guest_mode/ha_guest_mode.db"
+
+ICONS = [
+    "mdi:lock","mdi:lock-open","mdi:key",
+    "mdi:shield-lock","mdi:shield-key","mdi:shield-check",
+    "mdi:account-lock","mdi:account","mdi:account-group",
+    "mdi:account-key","mdi:key-variant","mdi:account-check",
+    "mdi:account-lock-outline","mdi:account-circle","mdi:link",
+    "mdi:link-variant","mdi:web","mdi:share-variant",
+    "mdi:star","mdi:bell","mdi:email"
+]
---
+++ custom_components/ha_guest_mode/options_flow.py
@@ -0,0 +1,31 @@
+import voluptuous as vol
+from homeassistant import config_entries
+
+from .const import ICONS 
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    """Handle options flow for Guest Mode."""
+
+    def __init__(self, config_entry):
+        self.config_entry = config_entry
+
+    async def async_step_init(self, user_input=None):
+        """Manage the options."""
+        if user_input is not None:
+            self.hass.config_entries.async_update_entry(
+                self.config_entry, options=user_input
+            )
+            await self.hass.config_entries.async_reload(self.config_entry.entry_id)
+            return self.async_create_entry(title="Guest Mode", data=self.config_entry.options)
+
+        tab_icon = self.config_entry.options.get("tab_icon", self.config_entry.data.get("tab_icon", "mdi:shield-key"))
+        tab_name = self.config_entry.options.get("tab_name", self.config_entry.data.get("tab_name", "Guest"))
+        path = self.config_entry.options.get("path_to_admin_ui", self.config_entry.data.get("path_to_admin_ui", "/guest-mode"))
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema({
+                vol.Optional("tab_icon", default=tab_icon): vol.In(ICONS),
+                vol.Optional("tab_name", default=tab_name): str,
+                vol.Optional("path_to_admin_ui", default=path): str,
+            }),
+        )
